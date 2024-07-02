fd2845849ecc50c1e1f6ae3add4c46291902189f
Ashu pateriya <107784054+ashuu7089@users.noreply.github.com>
2024-07-02 10:55:04 +0000

    01_ basic is running

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let accountEmail = "gamil.com"
+var accountName = "SBI"
+accountCity = "Indore"
+let accountState ;
+
+
+//accountId = 2 // mot allowed
+
+accountEmail = "xyz@gmail.com";
+accountName = "PNB"
+accountCity = "Punjab"
+console.table([accountEmail, accountName, accountCity, accountState]);
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,3 @@
+"use strict"; //treat all Js code as a newer version
+
+//alert("hello") // we are using node js not browser.
