ee2138b419d279fd50964ebd168520f197f10a3d
Ashu pateriya <107784054+ashuu7089@users.noreply.github.com>
2024-07-02 12:50:29 +0000

    DataType

--- 01_basic/02_dataType.js
+++ 01_basic/02_dataType.js
@@ -1,3 +1,18 @@
 "use strict"; //treat all Js code as a newer version
 
 //alert("hello") // we are using node js not browser.
+
+
+//number  => 2 power 53
+// bigInt 
+// string 
+// boolean  => true
+// null  => standalone value
+// undefined => 
+// Symbol => unique     
+
+
+// Object 
+console.log(typeof( null ));   // Object
+console.log(typeof undefined); // undefined
+
