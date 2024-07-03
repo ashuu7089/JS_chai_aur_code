2ebfe067637341d6c76a7644dc09420c4bb58167
Ashu pateriya <107784054+ashuu7089@users.noreply.github.com>
2024-07-03 07:36:29 +0000

    update operation with output.

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -2,34 +2,34 @@ let score = 33;
  
 let checkNumber = "33abc"
 // const { scored } = req.body;
-console.log(typeof checkNumber); // number
+// console.log(typeof checkNumber); // number
 
 let valueInNumber = Number(checkNumber)
-console.log( typeof valueInNumber, " 2 check Number"); // number
-console.log(valueInNumber, " "); // NaN
+// console.log( typeof valueInNumber, " 2 check Number"); // number
+// console.log(valueInNumber, " "); // NaN
 
-console.log(typeof score); // string
-console.log("*************************** Null *********************************");
+// console.log(typeof score); // string
+// console.log("*************************** Null *********************************");
 
 let data = null
-console.log(data);
-console.log(typeof data); //Object
+//console.log(data ,"ghfgfdg");
+// console.log(typeof data); //Object
 
 
 let valueForData = Number(data)
-console.log( typeof valueForData, " check Number"); // number
-console.log(valueForData, " "); // 0
-console.log("====================================Undefined=====================");
+// console.log( typeof valueForData, " check Number"); // number
+// console.log(valueForData, " "); // 0
+// console.log("====================================Undefined=====================");
 let checkForUndefined = undefined;
 
 
-console.log(checkForUndefined);
-console.log(typeof checkForUndefined); //undefined
+// console.log(checkForUndefined);
+// console.log(typeof checkForUndefined); //undefined
 
 
 let valueForcheckForUndefined = Number(checkForUndefined)
-console.log( typeof valueForcheckForUndefined, " check Number"); // number
-console.log(valueForcheckForUndefined, " "); //NaN
+// console.log( typeof valueForcheckForUndefined, " check Number"); // number
+// console.log(valueForcheckForUndefined, " "); //NaN
 
 
 
@@ -38,11 +38,26 @@ console.log(valueForcheckForUndefined, " "); //NaN
     true = 1
 */
 
-console.log("################## Boolean #########################");
+// console.log("################## Boolean #########################");
 let isLoggedIn = " ";
 let booleanIsLoggedIn  = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //  1 =>  true; 0 => false
 //  "" => false
     // "hitesh" => true
+
+
+    // *********************************** Operations *************************************
+    let value = 3;
+    let negValue = -value
+    console.log(negValue);
+    console.log(1 + "2");  // 12
+    console.log("2" + 3);  // 23
+    console.log("2" - 1);  //  1
+    console.log(1 + 2 + "2"); // 32
+    console.log("1" + 2  + "3"); // 123
+    console.log("2" * 2); //4
+    console.log("5" + null); // 5null
+    console.log(null + "5"); //  null5
+    console.log(null + 5); // 5
