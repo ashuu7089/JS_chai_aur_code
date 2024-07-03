c41f720477bf3035ff36dadbc2090e276f851324
Ashu pateriya <107784054+ashuu7089@users.noreply.github.com>
2024-07-03 06:34:06 +0000

    conversion of data type

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -1,3 +1,48 @@
 let score = 33; 
-const { scored } = req.body;
+ 
+let checkNumber = "33abc"
+// const { scored } = req.body;
+console.log(typeof checkNumber); // number
 
+let valueInNumber = Number(checkNumber)
+console.log( typeof valueInNumber, " 2 check Number"); // number
+console.log(valueInNumber, " "); // NaN
+
+console.log(typeof score); // string
+console.log("*************************** Null *********************************");
+
+let data = null
+console.log(data);
+console.log(typeof data); //Object
+
+
+let valueForData = Number(data)
+console.log( typeof valueForData, " check Number"); // number
+console.log(valueForData, " "); // 0
+console.log("====================================Undefined=====================");
+let checkForUndefined = undefined;
+
+
+console.log(checkForUndefined);
+console.log(typeof checkForUndefined); //undefined
+
+
+let valueForcheckForUndefined = Number(checkForUndefined)
+console.log( typeof valueForcheckForUndefined, " check Number"); // number
+console.log(valueForcheckForUndefined, " "); //NaN
+
+
+
+/*  "33" = 33
+    "33abc" = NaN
+    true = 1
+*/
+
+console.log("################## Boolean #########################");
+let isLoggedIn = " ";
+let booleanIsLoggedIn  = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//  1 =>  true; 0 => false
+//  "" => false
+    // "hitesh" => true
